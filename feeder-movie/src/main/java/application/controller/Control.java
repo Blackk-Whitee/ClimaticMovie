ad1bc916012df5bb6cc11f234f4cdb2e444fe39e
Aimar Alejandro <aimarsaal@gmail.com>
2025-05-18 12:25:33 +0100

    Changes in the pom.xml

--- feeder-movie/src/main/java/application/controller/Control.java
+++ feeder-movie/src/main/java/application/controller/Control.java
@@ -29,7 +29,7 @@ public class Control {
                 .build();
         Trigger trigger = TriggerBuilder.newTrigger()
                 .withIdentity("dailyTrigger", "group1")
-                .withSchedule(CronScheduleBuilder.dailyAtHourAndMinute(19, 55))
+                .withSchedule(CronScheduleBuilder.dailyAtHourAndMinute(12, 22))
                 .build();
         Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
         scheduler.start(); scheduler.scheduleJob(job, trigger);
